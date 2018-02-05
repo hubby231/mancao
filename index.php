d91ec6631c437daaeff1dff14381382fb075387e
hubby231 <36173191+hubby231@users.noreply.github.com>
2018-02-06 05:41:13 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,41 +1,31 @@
+<!DOCTYPE html>
 <html>
 <head>
 <style>
 h1{
- color:pink;
- font-family:verdana;
- font-size:200%;
- text-align:center;
-}
+   color:blue;
+   font-family:anaphora;
+   text-align:center;
+} 
 p1{
- color:blue;
- font-family:courier;
- font-size:120%;
+   color:yellow;
+   font-family:arial;
 }
-h2{
- color:blue;
- text:align:center;
+h2{ 
+
+   color:green;
+   text-align:center;
+   font-family:verdana;
 }
 p2{
- color:green;
+   color:orange;
 
-}
 </style>
 </head>
-<body>
-
-<h1>BASIC INFORMATION</h1>
-
-<p1>Name:Stephanie V. Manloloyo<br>
-Address:Campaog,Pitalo,San Fernando,Cebu<br>
-Contact #:09369109479<br>
-Email:stephaniemanloloyo4@gmail.com<br>
-Age:22 year's old<br>
-Date of Birth:March 14,1995<br>
-Gender:Female<br>
-Hobbies:cooking and dancing<br>
-Favorite Color:black,white,blue and red<br>
-</p1><h2><center>EDUCATIONAL BACKGROUND</h2><p2><center>
+<body style="background-color:powderblue;">
+<h1>Basic Information</h1><p1>Name:Mancao,Geneva S.<br>Address:Pitalo,San Fernando,Cebu<br>Contact:09750673174<br>
+Email:geneva.mancao@gmail.com<br>Age:24 years old<br>DOB:May 6,1993</p1>
+<h2><center>EDUCATIONAL BACKGROUND</h2><p2><center>
 <table style="width:100%";color:blue;>
 <tr>
 <th>School</th>
@@ -45,23 +35,18 @@ Favorite Color:black,white,blue and red<br>
 <tr>
 <td><center>Pitalo Elementary School</center></td>
 <td><center>Pitalo,San Fernando,Cebu</center></td>
-<td><center>2006-2007</center></td>
+<td><center>2005-2006</center></td>
 <tr>
-<td><center>Placido L. Senor National High School</center></td>
-<td><center>Langtad,City of Naga,Cebu</center></td>
+<td><center>Pitalo National High School</center></td>
+<td><center>Pitalo,San Fernando,Cebu</center></td>
 <td><center>2010-2011</center></td>
 </tr>
-</table>
-</p1><h2><center>BIOGRAPHY</h2><p2><center>
-I'm Stephanie V. Manloloy,22 years of age .Living in Campaog,Pitalo,San Fernando,Cebu.
-Currently studying in Cebu Technological University(Naga-Campus).I have five siblings three
-brother's and two sister's.My mother's name is Jonaliza Manloloyo she is a housewife.My fathers name is Emmanuel Manloloyo he is a motor driver.My hobbies are cooking and dancing.
-My favorite color are black,white,blue and red.<br>
-
- 
+</table></p1>
+<h2>Biography</h2><p2><center>Im Geneva S.Mancao.24 years old.I live in Pitalo San Fernando,Cebu.
+I have two brothers and two sisters, Im the fourth among them.I study at Cebu Technological University.
+My fathers name is Felix O. Mancao he is a foreman carpenter.My mothers name is Elvira S.Mancao
+, she is a plain housewife.My goal is to finish my study and to have a stable job in abroad.</p2>
 
 
-
-
-
-
+</body>
+</html>
